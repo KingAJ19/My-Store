5cc1d9344045d2a3ea719f980448e794ab005014
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2021-03-12 15:11:00 +0200

    added products.js page (still need to finish it)

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,7 +1,15 @@
 const data = {
     products: [
-        {
-            // Need to add all the data for each product yassis
+        { 
+            _id: '1',
+            name: 'Kobe Bryant Jersey',
+            category: 'NBA Jerseys',
+            image: './images/Jerseys/Basketball/Kobe-Jersey.jpg',
+            price: 400,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Kobe Bryant #24 Jersey'
         }
     ]
 }
