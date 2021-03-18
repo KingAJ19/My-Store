6d4eb8eda9f984ac96f57d42025aff77bbca5d0b
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2021-03-18 05:52:51 +0200

    added stars and reviews and installed react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -6,37 +7,18 @@ function App() {
     <div className="grid-container">
         <header className="row">
             <div>
-                <a className="brand" href="index.html">Anele's Store</a>
+                <a className="brand" href="/">Anele's Store</a>
             </div>
             <div>
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
             </div>
         </header>
         <main>
             <div className="row center">
                 {
                     data.products.map(product => (
-                        <div key={product._id} className="card">
-                            <a href={`/product/${product._id}`}>
-                                <img className="medium" src={product.image} alt={product.name} />
-                            </a>
-                            <div className="card-body">
-                            <a href={`/product/${product._id}`}>
-                                    <h2>{product.name}</h2>
-                                </a>
-                                <div className="rating">
-                                    <span> <i className="fa fa-star"></i></span>
-                                    <span><i className="fa fa-star"></i></span>
-                                    <span> <i className="fa fa-star"></i></span>
-                                    <span> <i className="fa fa-star"></i></span>
-                                    <span><i className="fa fa-star"></i></span>
-                                </div>
-                                <div className="price">
-                                    R{product.price}
-                                </div>
-                            </div>
-                      </div>
+                        <Product key={product._id} product={product}></Product>
                     ))
                 }
             </div>
