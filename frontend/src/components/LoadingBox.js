24a0ccc4dc9c81e02642229236ea4d96f2d40375
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2021-03-19 12:27:58 +0200

    fetch data with loading and error message box

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LoadingBox(){
+    return (
+        <div>
+            <center>
+                <br/>
+                <br/>
+                <i className="fa fa-spinner fa-spin"></i><b>LOADING....</b>
+            </center>
+        </div>
+    )
+} 
